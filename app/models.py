2bc69dca65f6c45db5a529169b8ed10a6cdffaf3
mpolinamm <minkovapmpm@gmail.com>
2025-05-26 17:54:24 +0200

    2025-05-26 PM

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from config import headers
 
 class Product:
 
-    def __init__(self, product_id, product_name, opinions, stats):
+    def __init__(self, product_id, product_name="", opinions=[], stats={}):
         self.product_id = product_id
         self.product_name = product_name
         self.opinions = opinions
