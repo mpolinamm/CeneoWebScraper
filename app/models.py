a34a0b94d74a6a898d57876bc711951568274ce1
mpolinamm <minkovapmpm@gmail.com>
2025-06-02 18:07:02 +0200

    2025-06-02 PM

--- app/models.py
+++ app/models.py
@@ -3,9 +3,13 @@ import json
 import requests
 import numpy as np
 import pandas as pd
+import matplotlib
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 from config import headers
+from app.utils import extract_data, translate_data, create_if_not_exist
+
+matplotlib.use('agg')
 
 class Product:
 
@@ -27,40 +31,65 @@ class Product:
     def extract_name(self):
         response = requests.get(self.get_link(), headers = headers)
         page_dom = BeautifulSoup(response.text, 'html.parser')
-        self.product_name = page_dom.select_one("h1")
+        self.product_name = extract_data(page_dom, "h1")
+
+    def extract_opinions(self):
+        url = self.get_link()
+        while url:
+            response = requests.get(url, headers = headers)
+            if response.status_code == 200:
+                print(url)
+                page_dom = BeautifulSoup(response.text, 'html.parser')
+                opinions = page_dom.select("div.js_product-review:not(.user-post--highlight)")
+                print(len(opinions))
+                for opinion in opinions:
+                    single_opinion = Opinion()
+                    single_opinion.extract(opinion).translate().transform()
+                    self.opinions.append(single_opinion)
+                try:
+                    url = "https://www.ceneo.pl"+extract_data(page_dom,"a.pagination__next","href")
+                except TypeError:
+                    url = None
 
     def opinions_to_dict(self):
         return [opinion.to_dict() for opinion in self.opinions]
+    
+    def info_to_dict(self):
+        return {
+            'product_id': self.product_id,
+            'product_name': self.product_name,
+            'stats': self.stats
+        }
 
     def calculate_stats(self):
         opinions = pd.DataFrame.from_dict(self.opinions_to_dict())
         self.stats["opinions_count"] = opinions.shape[0]
-        self.stats["pros_count"] = opinions.pros_pl.astype(bool).sum()
-        self.stats["cons_count"] = opinions.cons_pl.astype(bool).sum()
-        self.stats["pros_cons_count"] = opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum()
+        self.stats["pros_count"] = int(opinions.pros_pl.astype(bool).sum())
+        self.stats["cons_count"] = int(opinions.cons_pl.astype(bool).sum())
+        self.stats["pros_cons_count"] = int(opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum())
         self.stats["average_stars"] = opinions.stars.mean()
-        self.stats["pros"] = opinions.pros_en.explode().value_counts
-        self.stats["cons"] = opinions.cons_en.explode().value_counts
-        self.stats["recommendations"] = opinions.recommend.value_counts(dropna=False).reindex([True, False, None], fill_value=0)
-        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arange(0.5,5.5,0.5)), fill_value=0)
+        self.stats["pros"] = opinions.pros_en.explode().value_counts().to_dict()
+        self.stats["cons"] = opinions.cons_en.explode().value_counts().to_dict()
+        self.stats["recommendations"] = opinions.recommend.value_counts(dropna=False).reindex([True, False, None], fill_value=0).to_dict()
+        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arange(0.5,5.5,0.5)), fill_value=0).to_dict()
     
     def generate_charts(self):
-        if not os.path.exists("./app/static/pie_charts"):
-            os.mkdir("./app/static/pie_charts")
-        if not os.path.exists("./app/static/bar_charts"):
-            os.mkdir("./app/static/bar_charts")
-        self.stats['recommendations'].plot.pie(
+        create_if_not_exist("./app/static/pie_charts")
+        create_if_not_exist("./app/static/bar_charts")
+        recommendations =pd.Series(self.stats['recommendations'], index=self.stats['recommendations'].keys())
+        recommendations.plot.pie(
             label = "",
             labels = ["Recommend", "Not recommend", "No opinion"],
-            colors = ["turquoise", "red", "pink"],
+            colors = ['turquoise', 'blue', 'magenta'],
             autopct = lambda r: f"{r:.1f}%" if r>0 else ""
-            )
+        )
         plt.title(f"Recommendations for product {self.product_id}\nTotal number of opinions: {self.stats['opinion_count']}")
         plt.savefig(f"./app/static/pie_charts/{self.product_id}.png")
         plt.close()
         plt.figure (figsize=(7,6))
-        ax = self.stars.plot.bar(
-            color =["violet" if s>3.5 else "forestgreen" if s<3 else "steelblue" for s in self.stars.index]
+        stars = pd.Series(self.stars['stars'])
+        ax = stars.plot.bar(
+            color =["violet" if s>3.5 else "forestgreen" if s<3 else "steelblue" for s in stars.index]
         )
         plt.bar_label(container=ax.containers[0])
         plt.xlabel("Number of stars")
@@ -70,7 +99,19 @@ class Product:
         plt.savefig(f"./bar_charts/{self.product_id}.png")
         plt.close()
 
-class Opinions:
+    def save_opinions(self):
+        create_if_not_exist("./app/data")
+        create_if_not_exist("./app/data/opinions")
+        with open(f"./app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump(self.opinions_to_dict(), jf, indent=4, ensure_ascii=False)
+
+    def save_info(self):
+        create_if_not_exist("./app/data")
+        create_if_not_exist("./app/data/products")
+        with open(f"./app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump(self.info_to_dict(), jf, indent=4, ensure_ascii=False)
+
+class Opinion:
     selectors = {
     'opinion_id':(None, "data-entry-id"),
     'author':("span.user-post__author-name",),
@@ -82,20 +123,26 @@ class Opinions:
     'up_votes':("button.vote-yes", "data-total-vote"),
     'down_votes':("button.vote-no", "data-total-vote"),
     'published':("span.user-post__published > time:nth-child(1)", "datetime"),
-    'purchased':("span.user-post__published > time:nth-child(2)", "datetime")
+    'purchased':("span.user-post__published > time:nth-child(2)", "datetime"),
+    'content_en': (),
+    'pros_en': (),
+    'cons_en': ()
 }
-    def __init__(self, opinion_id, author, recommend, stars, content, pros, cons, up_votes, down_votes, published, purchased):
+    def __init__(self, opinion_id="", author="", recommend=False, stars=0, content_pl="", pros_pl=[], cons_pl=[], up_votes=0, down_votes=0, published=None, purchased=None, content_en="", pros_en=[], cons_en=[]):
         self.opinion_id = opinion_id
         self.author = author
         self.recommend = recommend
         self.stars = stars
-        self.content = content
-        self.pros = pros
-        self.cons = cons
+        self.content_pl = content_pl
+        self.pros_pl = pros_pl
+        self.cons_pl = cons_pl
         self.up_votes = up_votes
         self.down_votes = down_votes
         self.published = published
         self.purchased = purchased
+        self.content_en = content_en
+        self.pros_en = pros_en
+        self.cons_en = cons_en
 
     def __str__(self):
         return "\n".join([f"{key}: {getattr(self,key)}" for key in self.selectors.keys()])
@@ -105,3 +152,22 @@ class Opinions:
     
     def to_dict(self):
         return {key: getattr(self,key) for key in self.selectors.keys()}
+
+    def extract(self, opinion):
+        for key, values in self.selectors.items():
+            setattr(self,key,extract_data(opinion, *values))
+
+    def translate(self):
+        self.content_en = translate_data(self.content_pl)
+        self.pros_en = [translate_data(pros) for pros in self.pros_pl]
+        self.cons_en = [translate_data(cons) for cons in self.cons_pl]
+        return self
+
+    def transform(self):
+        self.recommend = True if self.recommend=="Polecam" else False if self.recommend =="Nie polecam" else None
+        self.stars= float(self.stars.split("/")[0].replace(",","."))
+        self.up_votes = int(self.up_votes)
+        self.down_votes = int(self.down_votes)
+        return self
+    
+    
